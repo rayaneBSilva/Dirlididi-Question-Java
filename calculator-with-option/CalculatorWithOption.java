d900a2587fe05ca4e747dc984bc8bc9c20569dda
RayaneSilva13 <rayane.silva@ccc.ufcg.edu.br>
2023-03-27 21:05:33 -0300

    adding questions

--- calculator-with-option/CalculatorWithOption.java
+++ calculator-with-option/CalculatorWithOption.java
@@ -1,4 +1,44 @@
+import java.util.Scanner;
 
 public class CalculatorWithOption {
-    
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    String menssage = "";
+    double result;
+
+    String operation = sc.nextLine();
+    if(validation(operation)){
+        double inputOne = sc.nextDouble();
+        double inputTwo = sc.nextDouble();
+        if(operation.equals("+")){
+            result = inputOne + inputTwo;
+            menssage = "RESULTADO " + result;
+        } else if(operation.equals("-")){
+            result = inputOne - inputTwo;
+            menssage = "RESULTADO " + result;
+        } else if (operation.equals("*")){
+            result = inputOne * inputTwo;
+            menssage = "RESULTADO " + result;
+        } else if (operation.equals("/")){
+            if(inputTwo == 0.0){
+                menssage = "ERRO";
+            }
+            result = inputOne / inputTwo;
+            menssage = "RESULTADO " + result;
+        }
+           
+    } 
+    sc.close();
+    System.out.println(menssage);
 }
+public static boolean validation(String operation){
+    boolean output = false;
+    if(operation.equals("+")) output = true;
+    if(operation.equals("-")) output = true;
+    if(operation.equals("/")) output = true;
+    if(operation.equals("*")) output = true;
+    return output;
+    }
+}
+    
+
