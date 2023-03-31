00a805e6608d2cd80d97a2326b53bc73774c9d04
RayaneSilva13 <rayane.silva@ccc.ufcg.edu.br>
2023-03-30 21:26:16 -0300

    adding question

--- beginning-volgal/BeginningVolgal.java
+++ beginning-volgal/BeginningVolgal.java
@@ -1,4 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class BeginningVolgal {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        List<String> result = new ArrayList<>();
+        int cont = 0;
+        while (cont < 5) {
+            String phase = sc.nextLine();
+            if (phase.charAt(0) == 'a') {
+                result.add("s");
+            } else {
+                result.add("n");
+            }
+            cont++;
+        }
+        for(int i = 0; i < result.size(); i++){
+            System.out.println(result.get(i));
+        }
+    }
 }
--- bigger-and-smaller/BiggerAndSmaller.java
+++ bigger-and-smaller/BiggerAndSmaller.java
@@ -1,4 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
-public class BiggerAndSmaller {
-    
+public class BiggerAndSmaller{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] numbers = new int[5];
+        for(int i = 0; i < 5 ; i++){
+            numbers[i] = sc.nextInt();
+        }
+
+        int bigger = numbers[0];
+        int smaller = numbers[0];
+
+        for(int i = 0; i < numbers.length; i++){
+            if(numbers[i] > bigger){
+                bigger = numbers[i];
+            } 
+            if(numbers[i] < smaller){
+                smaller = numbers[i];
+            }
+        }
+        System.out.println(bigger - smaller);
+        sc.close();
+      
+    }
 }
